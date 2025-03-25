6aa86e0a553a30da2233bb8afbdb059f44a0d932
yukehan <yukehan@yukehandeMac-mini.local>
2025-03-25 23:18:48 +0800

    Initial commit

---
+++ src/test/java/com/project/ad/AdSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdSpringCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
